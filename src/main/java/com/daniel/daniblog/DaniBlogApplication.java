364fb7cd65fd6b9cd2422d06f91cf1d33f208e3f
Daniel <dch950429@126.com>
2018-12-25 17:24:39 +0800

    Build the basic architect of the blog.

---
+++ src/main/java/com/daniel/daniblog/DaniBlogApplication.java
@@ -0,0 +1,14 @@
+package com.daniel.daniblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaniBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaniBlogApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/daniel/daniblog/DaniBlogApplicationTests.java
@@ -0,0 +1,17 @@
+package com.daniel.daniblog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DaniBlogApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
